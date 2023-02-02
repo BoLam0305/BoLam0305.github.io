b94186c77517430ac23c9900f3ecf87034491dbd
BoLam0305 <lamwaikit0305@gmail.com>
2023-02-03 00:57:04 +0800

    Initial commit

--- info.js
+++ info.js
@@ -1,6 +1,25 @@
-//Responsive layout
-window.addEventListener('resize', function (event) {
+const getDeviceType = () => {
+  const ua = navigator.userAgent;
+  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+    return "tablet";
+  }
+  if (
+    /Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+      ua
+    )
+  ) {
+    return "mobile";
+  }
+  return "desktop";
+};
 
+if (getDeviceType() === "mobile"){
+
+}
+
+//Responsive layout
+
+window.addEventListener('resize', function (event) {
 
     //responsive table
     if (window.innerWidth < 780) {
